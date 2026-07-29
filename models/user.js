fa10fdb3210193d917585209705ee3c7b27c3335
Kysohee18 <hanashahky@gmail.com>
2026-07-29 09:40:42 +0700

    Fix user model definition by adding email field with validation

--- models/user.js
+++ models/user.js
@@ -5,7 +5,11 @@ module.exports = (sequelize, DataTypes) => {
             primaryKey: true,
             autoIncrement: true
         },
-        
+        email: {
+            type: DataTypes.STRING,
+            allowNull: false,
+            unique: true
+        },
         password: {
             type: DataTypes.STRING,
             allowNull: false
