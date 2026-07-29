63bce93e343e11cd77312dd859d14074ddc39670
Kysohee18 <hanashahky@gmail.com>
2026-07-29 09:41:26 +0700

    Add API routes for user registration, login, and komik retrieval

---
+++ routes/api.js
@@ -0,0 +1,15 @@
+const express = require('express');
+const router = express.Router();
+const komikcontroller = require('../controller/komikController');
+const usercontroller = require("../controller/userController");
+const authMiddleware = require ("../middleware/authMiddleware");
+
+router.post("/register", usercontroller.register);
+router.post("/login", usercontroller.login);
+
+//public
+router.get('/komik',komikcontroller.getAllKomik);
+router.get('/komik/:id',komikcontroller.getKomikById);
+//private
+
+module.exports = router;
